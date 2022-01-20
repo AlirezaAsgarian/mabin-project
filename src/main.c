2482771fae4d8320ba7f7f1bb01a221edc142367
Alireza Askarian <ar.asgarian1389@gmail.com>
2022-01-20 14:00:24 +0330

    first commit of project

---
+++ src/main.c
@@ -0,0 +1,147 @@
+//Be name khoda
+#include<stdio.h>
+#include <SDL.h>       
+#include<stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include<stdlib.h>
+#include<time.h>
+enum window{menu,select_naghse,tasadof,bazi};
+int cmp(const void *a,const void *b)
+{
+
+int x =	(rand()%3) ;
+if(x > 1){return 1;}
+else{return -1;}
+}
+int main(int argc, char ** argv)
+{
+    bool quit = false;
+    SDL_Event event;
+    SDL_Init(SDL_INIT_EVERYTHING);
+    srand(time(NULL));
+    int pointsOfMap[]={1,2,3,4,5,6,7,8,9,10,11,12};
+    int tedad=rand()%6+6;
+    qsort(pointsOfMap,12,sizeof(int),cmp);
+    SDL_Window * window = SDL_CreateWindow("SDL2 Displaying Image",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1080, 554, 0);
+    SDL_Renderer *renderer1 = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer *renderer2 = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+   
+     #include</home/alireza/state.io/src/naghshe1.c>
+     SDL_Rect bakhsh1; bakhsh1.x=0; bakhsh1.y=0; bakhsh1.w=270; bakhsh1.h=185;
+     SDL_Rect bakhsh2; bakhsh2.x=270; bakhsh2.y=0; bakhsh2.w=270; bakhsh2.h=185;
+     SDL_Rect bakhsh3; bakhsh3.x=540; bakhsh3.y=0; bakhsh3.w=270; bakhsh3.h=185;
+     SDL_Rect bakhsh4; bakhsh4.x=810; bakhsh4.y=0; bakhsh4.w=270; bakhsh4.h=185;
+     SDL_Rect bakhsh5; bakhsh5.x=0; bakhsh5.y=185; bakhsh5.w=270; bakhsh5.h=185;
+     SDL_Rect bakhsh6; bakhsh6.x=270; bakhsh6.y=185; bakhsh6.w=270; bakhsh6.h=185;
+     SDL_Rect bakhsh7; bakhsh7.x=540; bakhsh7.y=185; bakhsh7.w=270; bakhsh7.h=185;
+     SDL_Rect bakhsh8; bakhsh8.x=810; bakhsh8.y=185; bakhsh8.w=270; bakhsh8.h=185;
+     SDL_Rect bakhsh9; bakhsh9.x=0;   bakhsh9.y=370; bakhsh9.w=270; bakhsh9.h=184;
+     SDL_Rect bakhsh10; bakhsh10.x=270; bakhsh10.y=370; bakhsh10.w=270; bakhsh10.h=184;
+     SDL_Rect bakhsh11; bakhsh11.x=540; bakhsh11.y=370; bakhsh11.w=270; bakhsh11.h=184;
+     SDL_Rect bakhsh12; bakhsh12.x=810; bakhsh12.y=370; bakhsh12.w=270; bakhsh12.h=184;
+     SDL_Rect bakhsh[12]={bakhsh1,bakhsh2,bakhsh3,bakhsh4,bakhsh5,bakhsh6,bakhsh7,bakhsh8,bakhsh9,bakhsh10,bakhsh11,bakhsh12};
+     SDL_SetRenderDrawColor(renderer2,255,0,0,255);
+     
+
+ int fpm= 60;
+ /*TTF_Init();
+  TTF_Font *font = TTF_OpenFont("LiberationSerif-Regular.ttf",20);
+  SDL_Color color = {0,255,0,255};
+  SDL_Surface *text = TTF_RenderText_Solid(font,"hello world",color);
+  SDL_Texture *message = SDL_CreateTextureFromSurface(renderer1,text);*/
+   SDL_bool done = true;
+int xm, ym;
+Uint32 buttons;
+
+SDL_Rect button;
+button.x=0; button.y=0; button.w=50; button.h=50;
+ //SDL_RenderFillRect(renderer2,&button);
+
+int iswindow= select_naghse+8;
+    while (!quit)
+    {
+      buttons = SDL_GetMouseState(&xm, &ym);
+      while(SDL_PollEvent(&event))
+      {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            quit = true;
+            break;
+            case SDL_MOUSEBUTTONDOWN :
+         #include </home/alireza/state.io/src/mouse settings.c>
+            break;
+        }
+      }
+
+        SDL_RenderClear(renderer1);
+      
+        
+      
+       
+        if(iswindow == -1){
+          #include</home/alireza/state.io/src/naghshe11.c> 
+          SDL_RenderPresent(renderer1);}
+        else if(iswindow == -2 ) { 
+
+        for(int i=0;i<tedad;++i)
+        {
+        SDL_RenderCopy(renderer1,*(distinct+i),NULL,&bakhsh[pointsOfMap[i]]);
+        }
+        SDL_RenderPresent(renderer1);
+        }
+        else if(iswindow == select_naghse+8)
+        {
+            #include</home/alireza/state.io/src/select_naghse.c>  
+        }
+        else 
+        {
+          SDL_RenderClear(renderer2); 
+          SDL_RenderPresent(renderer2);
+
+        }
+        printf("iswindow = %d \n",iswindow);
+        SDL_Delay(1000/fpm);
+
+    }
+    	
+    SDL_DestroyTexture(tex);
+    SDL_DestroyTexture(te1);
+    SDL_DestroyTexture(te2);
+    SDL_DestroyTexture(te3);
+    SDL_DestroyTexture(te4);
+    SDL_DestroyTexture(te5);
+    SDL_DestroyTexture(te6);
+    SDL_DestroyTexture(te7);
+    SDL_DestroyTexture(te8);
+    SDL_DestroyTexture(te9);
+    SDL_DestroyTexture(te10);
+    SDL_DestroyTexture(te11);
+    SDL_DestroyTexture(te12);
+    SDL_DestroyTexture(te13);
+    SDL_DestroyTexture(te14);
+    SDL_DestroyTexture(te15);
+    SDL_DestroyTexture(te16);
+    SDL_DestroyTexture(te17);
+    SDL_DestroyTexture(te18);
+    SDL_DestroyTexture(te19);
+    SDL_DestroyTexture(te20);
+    SDL_DestroyTexture(te21);
+    SDL_DestroyTexture(te22);
+    SDL_DestroyTexture(te23);
+    SDL_DestroyTexture(te24);
+    SDL_DestroyTexture(te25);
+    SDL_DestroyTexture(te26);
+    SDL_DestroyTexture(te27);
+    
+
+    
+    
+    SDL_Quit();
+ 
+    return 0;
+}
+
---
+++ src/select_naghse.c
@@ -0,0 +1,95 @@
+
+
+
+
+    SDL_Surface *n1 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/نقشه 1.png");
+    SDL_Surface *n2 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/نقشه 2.png");
+    SDL_Surface *n3 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/نقشه 3.png");
+    SDL_Surface *n4 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/نقشه 4.png");
+    SDL_Surface *b1 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/b1.jpeg");
+    SDL_Surface *b2 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/b2.jpeg");
+    SDL_Surface *b3 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/b3.jpeg");
+    SDL_Surface *b4 = IMG_Load("/home/alireza/state.io/src/انتخاب نقشه/b4.jpeg");
+    SDL_Surface *naghshe[]={n1,n2,n3,n4};
+    SDL_Surface *bu[]={b1,b2,b3,b4};
+    SDL_Texture **whichn =(SDL_Texture **)malloc(4*sizeof(SDL_Texture*));
+     for(int i=0;i<4;++i)
+     {
+             *(whichn+i) = (SDL_Texture *)malloc(sizeof(SDL_Texture*));
+             *(whichn+i)= SDL_CreateTextureFromSurface(renderer1,naghshe[i]);
+             if(*(whichn+i) == NULL){printf("can't %s",SDL_GetError());}
+     }
+     SDL_Texture **buT =(SDL_Texture **)malloc(4*sizeof(SDL_Texture*));
+     for(int i=0;i<4;++i)
+     {
+             *(buT+i) = (SDL_Texture *)malloc(sizeof(SDL_Texture*));
+             *(buT+i)= SDL_CreateTextureFromSurface(renderer1,bu[i]);
+             if(*(bu+i) == NULL){printf("can't %s",SDL_GetError());}
+     }
+        SDL_FreeSurface(n1);
+        SDL_FreeSurface(n2);
+        SDL_FreeSurface(n3);
+        SDL_FreeSurface(n4);
+        SDL_FreeSurface(b1);
+        SDL_FreeSurface(b2);
+        SDL_FreeSurface(b3);
+        SDL_FreeSurface(b4);
+        SDL_Rect dis1;
+        dis1.x=0; dis1.y=0; dis1.w =1080; dis1.h=554;
+        SDL_Rect b1r;
+        b1r.x=208; b1r.y=262; b1r.w =50; b1r.h=50;
+        SDL_Rect b2r;
+        b2r.x=497; b2r.y=219; b2r.w =50; b2r.h=50;
+        SDL_Rect b3r;
+        b3r.x=852; b3r.y=288; b3r.w =50; b3r.h=50;
+        SDL_Rect b4r;
+        b4r.x=541; b4r.y=431; b4r.w =50; b4r.h=50;
+
+           
+          
+        printf("x=%d y=%d\n",xm,ym);
+
+        if(xm>211 && xm<258 && ym>264 && ym<310)
+        {
+                   
+          SDL_RenderCopy(renderer1,*(whichn),NULL,&dis1);
+
+           
+        } 
+        else if(xm>499 && xm<546 && ym>200 && ym<268)
+        {
+            SDL_RenderCopy(renderer1,*(whichn+1),NULL,&dis1);
+
+        }              
+        else if(xm>852 && xm<902 && ym>288 && ym<338)
+        {
+
+
+        SDL_RenderCopy(renderer1,*(whichn+2),NULL,&dis1);
+
+
+
+        }
+          else if(xm>541 && xm<591 && ym>431 && ym<481)
+          {
+
+           SDL_RenderCopy(renderer1,*(whichn+3),NULL,&dis1);
+
+          }
+          else 
+          {
+          // SDL_Texture *defaultT = SDL_CreateTextureFromSurface(renderer1,image);
+            SDL_RenderCopy(renderer1,tex,NULL,&dis1);
+          }
+           SDL_RenderCopy(renderer1,*(buT),NULL,&b1r);
+           SDL_RenderCopy(renderer1,*(buT+1),NULL,&b2r);
+           SDL_RenderCopy(renderer1,*(buT+2),NULL,&b3r);
+           SDL_RenderCopy(renderer1,*(buT+3),NULL,&b4r);
+          SDL_RenderPresent(renderer1);
+        for(int i=0;i<4;++i)
+     {
+    SDL_DestroyTexture(*(buT+i));
+    SDL_DestroyTexture(*(whichn+i));
+     }
+     //akjljoaiaijf 
+     
